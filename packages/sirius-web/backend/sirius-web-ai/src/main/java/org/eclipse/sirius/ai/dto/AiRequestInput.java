252edb411c065114bc314a9ed514b906edb64b87
LenaB-obeo <lena.bamouh@obeosoft.com>
2025-01-31 11:01:04 +0100

    feat: requests to an LLM (GPT-4o) through HTTP Post

---
+++ packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/ai/dto/AiRequestInput.java
@@ -0,0 +1,10 @@
+package org.eclipse.sirius.ai.dto;
+
+import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramLayoutDataInput;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.LayoutDiagramInput;
+import org.eclipse.sirius.components.core.api.IInput;
+
+import java.util.UUID;
+
+public record AiRequestInput(UUID id, String request, String editingContextId, String representationId, DiagramLayoutDataInput diagramLayoutDataInput) implements IInput {
+}
