e13e102b89d7cb0e6e90208e2db78723d155d9bf
LenaB-obeo <lena.bamouh@obeosoft.com>
2025-02-06 17:21:02 +0100

    refacto: using var instead of explicit typing

--- packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/ai/util/JsonParser.java
+++ packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/ai/util/JsonParser.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class JsonParser {
     public static Map<String, Object> parseJsonToMap(String jsonString) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
+        var objectMapper = new ObjectMapper();
         return objectMapper.readValue(jsonString, Map.class);
     }
 }
