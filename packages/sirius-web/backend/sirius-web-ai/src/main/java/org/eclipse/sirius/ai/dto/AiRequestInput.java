21abda83643a9c45ea7568e63d5447187512da1f
LenaB-obeo <lena.bamouh@obeosoft.com>
2025-02-06 17:00:35 +0100

    refacto: code organisation, performance improvement

--- packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/ai/dto/AiRequestInput.java
+++ packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/ai/dto/AiRequestInput.java
@@ -1,10 +1,8 @@
 package org.eclipse.sirius.ai.dto;
 
-import org.eclipse.sirius.components.collaborative.diagrams.dto.DiagramLayoutDataInput;
-import org.eclipse.sirius.components.collaborative.diagrams.dto.LayoutDiagramInput;
 import org.eclipse.sirius.components.core.api.IInput;
 
 import java.util.UUID;
 
-public record AiRequestInput(UUID id, String request, String editingContextId, String representationId, DiagramLayoutDataInput diagramLayoutDataInput) implements IInput {
+public record AiRequestInput(UUID id, String request, String editingContextId, String representationId) implements IInput {
 }
