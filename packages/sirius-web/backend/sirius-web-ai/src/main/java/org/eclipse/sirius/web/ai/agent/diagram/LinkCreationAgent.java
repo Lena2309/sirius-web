31974b07eb3ceacdb717337addec13cf57e63859
LenaB-obeo <lena.bamouh@obeosoft.com>
2025-03-14 10:44:58 +0100

    refacto: renaming of ReasonAgent into PromptInterpreter to reflect its true behaviour (non agentic)

--- packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/web/ai/agent/diagram/LinkCreationAgent.java
+++ packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/web/ai/agent/diagram/LinkCreationAgent.java
@@ -7,6 +7,7 @@ import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.request.ChatRequest;
 import dev.langchain4j.model.chat.request.ChatRequestParameters;
+import org.eclipse.sirius.web.ai.agent.diagram.edition.LinkEditionAgent;
 import org.eclipse.sirius.web.ai.configuration.AiModelsConfiguration;
 import org.eclipse.sirius.web.ai.dto.AgentResult;
 import org.eclipse.sirius.web.ai.service.ToolCallService;
@@ -15,6 +16,8 @@ import org.eclipse.sirius.web.ai.tool.creation.LinkCreationTools;
 import org.eclipse.sirius.components.core.api.IInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -30,10 +33,17 @@ public class LinkCreationAgent implements DiagramAgent {
 
     private final List<AiTool> toolClasses = new ArrayList<>();
 
+    private final LinkEditionAgent linkEditionAgent;
+
+    private final ThreadPoolTaskExecutor taskExecutor;
+
     private IInput input;
 
-    public LinkCreationAgent(LinkCreationTools linkCreationTools) {
-        this.model = AiModelsConfiguration.buildChatModel(DIAGRAM);
+    public LinkCreationAgent(LinkCreationTools linkCreationTools, LinkEditionAgent linkEditionAgent,
+                             @Qualifier("threadPoolTaskExecutor") ThreadPoolTaskExecutor taskExecutor) {
+        this.model = AiModelsConfiguration.buildChatModel(DIAGRAM).get();
+        this.linkEditionAgent = linkEditionAgent;
+        this.taskExecutor = taskExecutor;
         this.toolClasses.add(linkCreationTools);
     }
 
@@ -49,16 +59,18 @@ public class LinkCreationAgent implements DiagramAgent {
         }
     }
 
-    @Tool("Links two objects together. Do not mention other properties here.")
-    public String linkObjects(@P("Explain what should be linked and why (briefly), and optionally how. Precise that the preferred type of link is optional.") String prompt, @P("The source object id, the id is in a format similar to \"AbcdEF+GhijKLM1NOpqrS==\".") String sourceObjectId, @P("The target object id, the id is in a format similar to \"AbcdEF+GhijKLM1NOpqrS==\".") String targetObjectId) throws UnsupportedOperationException {
+    @Tool("Links two objects together.")
+    public String linkObjects(@P("Explain what should be linked and why. Mention special properties if necessary.") String prompt, @P("The source object id, the id is in a format similar to \"AbcdEF+GhijKLM1NOpqrS==\".") String sourceObjectId, @P("The target object id, the id is in a format similar to \"AbcdEF+GhijKLM1NOpqrS==\".") String targetObjectId) throws UnsupportedOperationException {
         var rateLimiter = AiModelsConfiguration.getRateLimiter(this.model);
-        var specifications = new ArrayList<>(initializeSpecifications(List.of(), this.input, this.toolClasses));
+        var specifications = new ArrayList<>(initializeSpecifications(List.of(this.linkEditionAgent), this.input, this.toolClasses));
         this.setToolsInput();
 
         var systemMessage = new SystemMessage("""
             You are an assistant for Diagram Object Linking.
             Do not write any text, just call the correct tools to link two diagram objects given in the user's request .
-            Do not hallucinate. When it is possible to link objects, link them, if possible with the user preferred link.
+            Do not hallucinate.
+            When it is possible to link objects, link them, if possible with the user preferred link.
+            If the preferred link is not available, you should still link the objects in the best way possible.
             """
         );
 
@@ -70,7 +82,9 @@ public class LinkCreationAgent implements DiagramAgent {
                 .build();
 
         var results = new ArrayList<AgentResult>();
-        ToolCallService.computeToolCalls(logger, this.model, chatRequest, this.toolClasses, results, rateLimiter);
+        //ToolCallService.computeToolCalls(logger, this.model, chatRequest, this.toolClasses, results, rateLimiter);
+
+        ToolCallService.computeToolCalls(logger, this.model, chatRequest, List.of(this.linkEditionAgent), this.toolClasses, results, this.taskExecutor, rateLimiter);
 
         return results.toString();
     }
