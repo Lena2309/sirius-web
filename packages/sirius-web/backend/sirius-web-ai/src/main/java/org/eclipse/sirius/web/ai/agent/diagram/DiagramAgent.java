4b6b346ed02d6656f073222b64c4d3eafbe1cd6d
LenaB-obeo <lena.bamouh@obeosoft.com>
2025-03-14 10:44:58 +0100

    refacto: renaming of ReasonAgent into PromptInterpreter to reflect its true behaviour (non agentic)

--- packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/web/ai/agent/diagram/DiagramAgent.java
+++ packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/web/ai/agent/diagram/DiagramAgent.java
@@ -4,6 +4,6 @@ import org.eclipse.sirius.web.ai.agent.Agent;
 import org.eclipse.sirius.components.core.api.IInput;
 
 public interface DiagramAgent extends Agent {
-    public void setInput(IInput input);
-    public void setToolsInput();
+    void setInput(IInput input);
+    void setToolsInput();
 }
