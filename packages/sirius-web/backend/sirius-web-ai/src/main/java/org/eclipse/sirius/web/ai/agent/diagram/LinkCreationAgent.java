0f22a0035b882d8e6711387e54410d8be84f328c
LenaB-obeo <lena.bamouh@obeosoft.com>
2025-03-21 14:12:23 +0100

    Migration to springai

--- packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/web/ai/agent/diagram/LinkCreationAgent.java
+++ packages/sirius-web/backend/sirius-web-ai/src/main/java/org/eclipse/sirius/web/ai/agent/diagram/LinkCreationAgent.java
@@ -1,21 +1,21 @@
 package org.eclipse.sirius.web.ai.agent.diagram;
 
-import dev.langchain4j.agent.tool.P;
-import dev.langchain4j.agent.tool.Tool;
-import dev.langchain4j.data.message.SystemMessage;
-import dev.langchain4j.data.message.UserMessage;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.chat.request.ChatRequest;
-import dev.langchain4j.model.chat.request.ChatRequestParameters;
 import org.eclipse.sirius.web.ai.agent.diagram.edition.LinkEditionAgent;
 import org.eclipse.sirius.web.ai.configuration.AiModelsConfiguration;
-import org.eclipse.sirius.web.ai.dto.AgentResult;
-import org.eclipse.sirius.web.ai.service.ToolCallService;
 import org.eclipse.sirius.web.ai.tool.AiTool;
 import org.eclipse.sirius.web.ai.tool.creation.LinkCreationTools;
 import org.eclipse.sirius.components.core.api.IInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
@@ -29,9 +29,11 @@ import static org.eclipse.sirius.web.ai.configuration.AiModelsConfiguration.Mode
 public class LinkCreationAgent implements DiagramAgent {
     private static final Logger logger = LoggerFactory.getLogger(LinkCreationAgent.class);
 
-    private final ChatLanguageModel model;
+    private final ChatModel model;
 
     private final List<AiTool> toolClasses = new ArrayList<>();
+
+    private final LinkCreationTools linkCreationTools;
 
     private final LinkEditionAgent linkEditionAgent;
 
@@ -45,6 +47,7 @@ public class LinkCreationAgent implements DiagramAgent {
         this.linkEditionAgent = linkEditionAgent;
         this.taskExecutor = taskExecutor;
         this.toolClasses.add(linkCreationTools);
+        this.linkCreationTools = linkCreationTools;
     }
 
     @Override
@@ -59,10 +62,9 @@ public class LinkCreationAgent implements DiagramAgent {
         }
     }
 
-    @Tool("Links two objects together.")
-    public String linkObjects(@P("Explain what should be linked and why. Mention special properties if necessary.") String prompt, @P("The source object id, the id is in a format similar to \"AbcdEF+GhijKLM1NOpqrS==\".") String sourceObjectId, @P("The target object id, the id is in a format similar to \"AbcdEF+GhijKLM1NOpqrS==\".") String targetObjectId) throws UnsupportedOperationException {
-        var rateLimiter = AiModelsConfiguration.getRateLimiter(this.model);
-        var specifications = new ArrayList<>(initializeSpecifications(List.of(this.linkEditionAgent), this.input, this.toolClasses));
+    @Tool(description ="Links two objects together.")
+    public String linkObjects(@ToolParam(description ="Explain what should be linked and why. Mention special properties if necessary.") String orchestratorPrompt, @ToolParam(description ="The source object id, the id is in a format similar to \"AbcdEF+GhijKLM1NOpqrS==\".") String sourceObjectId, @ToolParam(description = "The target object id, the id is in a format similar to \"AbcdEF+GhijKLM1NOpqrS==\".") String targetObjectId) throws UnsupportedOperationException {
+        initializeSpecifications(List.of(this.linkEditionAgent), this.input);
         this.setToolsInput();
 
         var systemMessage = new SystemMessage("""
@@ -74,18 +76,12 @@ public class LinkCreationAgent implements DiagramAgent {
             """
         );
 
-        var chatRequest = ChatRequest.builder()
-                .messages(List.of(systemMessage, new UserMessage("Here is the source diagram object id: " + sourceObjectId + " and here is the target object id: " + targetObjectId + ". " + prompt)))
-                .parameters(ChatRequestParameters.builder()
-                        .toolSpecifications(specifications)
-                        .build())
+        var chatClient = ChatClient.builder(this.model)
+                .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
                 .build();
 
-        var results = new ArrayList<AgentResult>();
-        //ToolCallService.computeToolCalls(logger, this.model, chatRequest, this.toolClasses, results, rateLimiter);
+        var prompt = new Prompt(systemMessage, new UserMessage("Here is the source diagram object id: " + sourceObjectId + " and here is the target object id: " + targetObjectId + ". " + orchestratorPrompt));
 
-        ToolCallService.computeToolCalls(logger, this.model, chatRequest, List.of(this.linkEditionAgent), this.toolClasses, results, this.taskExecutor, rateLimiter);
-
-        return results.toString();
+        return chatClient.prompt(prompt).tools(linkEditionAgent, linkCreationTools).call().content();
     }
 }
